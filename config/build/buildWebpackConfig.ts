437dd7afd8c73fee49b3f654f04e7e1faa282293
waldo <evgenijtser@gmail.com>
2023-01-30 19:02:49 +0300

    Add config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import webpack from "webpack";
+import {BuildOptions} from "./types/config";
+import path from "path";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+import {buildPlugins} from "./buildPlugins";
+import {buildDevServer} from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const {paths, mode, isDev} = options
+    return {
+        mode: mode,
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true
+        },
+        module: {
+            rules: buildLoaders(options)
+        },
+        resolve: buildResolvers(),
+        plugins: buildPlugins(options),
+        devtool: isDev ? "inline-source-map" : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined
+    }
+}
