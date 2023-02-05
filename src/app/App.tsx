a3bbf46a985a127f3ca39fa4f243e417df772deb
waldo <evgenijtser@gmail.com>
2023-02-05 11:12:45 +0300

    Переписал на FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import {Link, Route, Routes} from "react-router-dom";
 import "./styles/index.scss"
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {Suspense, useContext, useState} from "react";
-import {Theme, ThemeContext} from "./theme/ThemeContext";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {Theme, ThemeContext} from "./providers/ThemeProvider/lib/ThemeContext";
+import {useTheme} from "./providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 
 const App = () => {
@@ -18,8 +18,8 @@ const App = () => {
             <Link to={"/about"}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={"/about"} element={<AboutPageAsync/>}/>
-                    <Route path={"/"} element={<MainPageAsync/>}/>
+                    <Route path={"/about"} element={<AboutPage/>}/>
+                    <Route path={"/"} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
