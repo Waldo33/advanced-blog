a46a223e31379fb919209c3e021b5bb9a1827718
waldo <evgenijtser@gmail.com>
2023-03-04 00:00:30 +0300

    Пофиксил отображение ошибки в форме, добавил асинхронные редьюсеры в StoreDecorator.tsx

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,15 +1,19 @@
 import { FC, ReactNode } from 'react';
 import { Provider } from 'react-redux';
-import { DeepPartial } from '@reduxjs/toolkit';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { StateSchema } from '../config/StateSchema';
 import { createReduxStore } from '../config/store';
 
 interface StoreProviderProps {
     children?: ReactNode;
     initialState?: DeepPartial<StateSchema>;
+    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
 }
-export const StoreProvider: FC<StoreProviderProps> = ({ children, initialState }) => {
-    const store = createReduxStore(initialState as StateSchema);
+export const StoreProvider: FC<StoreProviderProps> = ({ children, initialState, asyncReducers }) => {
+    const store = createReduxStore(
+        initialState as StateSchema,
+        asyncReducers as ReducersMapObject<StateSchema>,
+    );
 
     return (
         <Provider store={store}>
