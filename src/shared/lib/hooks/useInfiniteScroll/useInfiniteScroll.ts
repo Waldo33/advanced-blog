94c2269110c87ca4fda9f38df2e7f6a3e33edc10
waldo <evgenijtser@gmail.com>
2023-04-22 12:44:24 +0300

    Исправил ошибку хука

--- src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -7,11 +7,13 @@ export interface UseInfiniteScrollProps {
 }
 
 export function useInfiniteScroll({ callback, triggerRef, wrapperRef }: UseInfiniteScrollProps) {
+    const wrapperElement = wrapperRef.current;
+    const triggerElement = triggerRef.current;
     useEffect(() => {
         let observer: IntersectionObserver | null = null;
         if (callback) {
             const options = {
-                root: wrapperRef.current,
+                root: wrapperElement,
                 rootMargin: '0px',
                 threshold: 1.0,
             };
@@ -26,10 +28,9 @@ export function useInfiniteScroll({ callback, triggerRef, wrapperRef }: UseInfin
         }
 
         return () => {
-            if (observer) {
-                // eslint-disable-next-line react-hooks/exhaustive-deps
-                observer.unobserve(triggerRef.current);
+            if (observer && triggerElement) {
+                observer.unobserve(triggerElement);
             }
         };
-    }, [callback, triggerRef, wrapperRef]);
+    }, [callback, triggerElement, triggerRef, wrapperElement, wrapperRef]);
 }
